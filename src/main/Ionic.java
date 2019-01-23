d7e7209c50a4ed890912fd2540a5cbf79c9775f3
JkyProgrammer <32058792+JkyProgrammer@users.noreply.github.com>
2019-01-23 07:26:59 +0000

    Worked on text analysis

--- src/main/Ionic.java
+++ src/main/Ionic.java
@@ -15,6 +15,7 @@ public class Ionic {
 	public static void main(String[] args) {
 		Ionic i = new Ionic ();
 		i.examineURL ("https://en.wikipedia.org/wiki/Logic_bomb");
+		i.examineURL("https://en.wikipedia.org/wiki/Gordon_Cowans");
 	}
 	
 	WikipediaGetter wg = new WikipediaGetter ();
@@ -53,6 +54,54 @@ public class Ionic {
 		String tmp = content.substring(0, indexOfContents);
 		content = tmp + content.substring(indexOfContentsEnd + 6);
 		
+		// Remove other unwanted tags
+		content = content.replaceAll("<b>", "");
+		content = content.replaceAll("</b>", "");
+		
+		// Search for and replace links in the content
+		int endOfContent = content.length()-1;
+		int searchIndex = content.indexOf("<a href=");
+		while (searchIndex <= endOfContent && searchIndex != -1) {
+			String suffix = content.substring(searchIndex + 9, content.indexOf ("\"", searchIndex + 9));
+			String linkFullText = content.substring(searchIndex, content.indexOf ("</a>", searchIndex)+4);
+			int ltStart = linkFullText.length()-6;
+			while (linkFullText.charAt(ltStart) != '>') {
+				ltStart--;
+			}
+			String linkText = linkFullText.substring (ltStart+1, linkFullText.length()-4);
+			
+			if (suffix.contains("action=edit")) {
+				content = content.replace(linkFullText, "");
+			} else if (suffix.contains("#cite_note-")) {
+				content = content.replace(linkFullText, "");
+			} else {
+				linksToSee.add("https://en.wikipedia.org" + suffix);
+				content = content.replace(linkFullText, linkText);
+			}
+			
+			searchIndex = content.indexOf("<a href=", searchIndex);
+			endOfContent = content.length()-1;
+		}
+		
+		// Analyse the text content
+		endOfContent = content.length()-1;
+		searchIndex = 0;
+		
+		WikipediaArticle currentArticle = new WikipediaArticle ();
+		currentArticle.title = startURL.substring(startURL.lastIndexOf("/")+1);
+		currentArticle.link = startURL;
+		WikipediaHeading currentHeading = new WikipediaHeading ("Summary");
+		
+//		while (searchIndex <= endOfContent && searchIndex != -1) { // TODO: Jump to next TAG, single nested loop
+//			while ((searchIndex = content.indexOf("<p>", searchIndex)) != -1 && searchIndex <= endOfContent) {
+//				String tmpStr = "";
+//				// TODO: Extract rendered text and links
+//			}
+//			currentArticle.headings.add(currentHeading);
+//			currentHeading = new WikipediaHeading ("<Next heading>"); // TODO: Find heading name
+//		}
+		
+		
 		System.out.println(content);
 		for (String s : linksToSee) {
 //			if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
@@ -65,24 +114,6 @@ public class Ionic {
 //				}
 //			}
 			System.out.println(s);
-		}
-		
-		// Analyse the text content
-		int endOfContent = content.length()-1;
-		int searchIndex = 0;
-		
-		WikipediaArticle currentArticle = new WikipediaArticle ();
-		currentArticle.title = startURL.substring(startURL.lastIndexOf("/")+1);
-		currentArticle.link = startURL;
-		WikipediaHeading currentHeading = new WikipediaHeading ("Summary");
-		
-		while (searchIndex <= endOfContent && searchIndex != -1) { // TODO: Jump to next TAG, single nested loop
-			while ((searchIndex = content.indexOf("<p>", searchIndex)) != -1 && searchIndex <= endOfContent) {
-				String tmpStr = "";
-				// TODO: Extract rendered text and links
-			}
-			currentArticle.headings.add(currentHeading);
-			currentHeading = new WikipediaHeading ("<Next heading>"); // TODO: Find heading name
 		}
 		
 		usingBufferedWriter (content, "page.html");
